c585081740f91668b94b6ae5bbb5977071bd7ff1
Mohammad Aatif Shaikh <78430955+aatif101@users.noreply.github.com>
2025-11-25 21:47:53 +0000

    first frame

---
+++ Frames/Frame1/Frame1.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+
+export const Frame = () => {
+  return (
+    <div
+      className="w-full min-w-[1390px] min-h-[902px] flex"
+      data-model-id="5:228"
+    >
+      <div className="w-[1390px] h-[902px] relative bg-[linear-gradient(180deg,rgba(245,235,224,1)_0%,rgba(253,246,237,1)_100%)]">
+        <header className="flex w-[335px] h-[164px] items-center justify-around gap-[826.15px] px-10 py-0 absolute top-[317px] left-[528px] bg-transparent">
+          <div className="flex w-[261px] h-20 items-start gap-4 relative mr-[-6.00px]">
+            <button className="all-[unset] box-border relative w-[115.73px] h-[54px] bg-[#f5ebe0] rounded-[16777200px] border-[3px] border-solid border-[#2b2622] shadow-[5px_5px_0px_#2b2622d9]">
+              <div className="absolute top-[18px] left-[38px] [font-family:'Space_Grotesk',Helvetica] font-bold text-[#2b2622] text-sm text-center tracking-[0.28px] leading-5 whitespace-nowrap">
+                LOGIN
+              </div>
+            </button>
+
+            <button className="all-[unset] box-border relative flex-1 grow h-[54px] bg-[#ef473a] rounded-[16777200px] border-[3px] border-solid border-[#2b2622] shadow-[5px_5px_0px_#2b2622d9]">
+              <div className="absolute top-[18px] left-[38px] [font-family:'Space_Grotesk',Helvetica] font-bold text-white text-sm text-center tracking-[0.28px] leading-5 whitespace-nowrap">
+                SIGN UP
+              </div>
+            </button>
+          </div>
+        </header>
+
+        <img
+          className="absolute top-[84px] left-[131px] w-[200px] h-[200px] aspect-[1] object-cover"
+          alt="Chatgpt image nov"
+          src="https://c.animaapp.com/NlUgQSjM/img/chatgpt-image-nov-24--2025--01-19-41-pm-png-1@2x.png"
+        />
+
+        <div className="absolute top-[139px] left-[calc(50.00%_-_472px)] w-[944px] h-[140px] flex">
+          <div className="mt-[5px] w-[736px] h-20 ml-[103.8px] [font-family:'Space_Grotesk',Helvetica] font-bold text-[#1e5f4d] text-[140px] text-center tracking-[-1.60px] leading-[80px] whitespace-nowrap">
+            PrepStreak
+          </div>
+        </div>
+
+        <div className="absolute top-[572px] left-[calc(50.00%_-_600px)] w-[1200px] h-64 flex gap-8">
+          <div className="w-[378.66px] h-[255.78px] flex flex-col bg-white rounded-3xl border-4 border-solid border-[#2b2622] shadow-[5px_5px_0px_#2b2622d9]">
+            <img
+              className="ml-9 w-20 h-20 relative mt-9"
+              alt="Container"
+              src="https://c.animaapp.com/NlUgQSjM/img/container.svg"
+            />
+
+            <div className="ml-9 w-[306.66px] h-[28.59px] mt-6 flex">
+              <div className="mt-[1.5px] w-[189px] h-[29px] [font-family:'Space_Grotesk',Helvetica] font-bold text-[#2b2622] text-[22px] tracking-[-0.22px] leading-[28.6px] whitespace-nowrap">
+                Gamified Learning
+              </div>
+            </div>
+
+            <div className="ml-9 w-[306.66px] h-[51.19px] flex">
+              <p className="-mt-px w-[293px] h-[52px] [font-family:'Inter',Helvetica] font-normal text-[#6b6661] text-base tracking-[0] leading-[25.6px]">
+                Tackle complex topics through engaging challenges and levels.
+              </p>
+            </div>
+          </div>
+
+          <div className="w-[378.66px] h-[255.78px] flex flex-col bg-white rounded-3xl border-4 border-solid border-[#2b2622] shadow-[5px_5px_0px_#2b2622d9]">
+            <img
+              className="ml-9 w-20 h-20 relative mt-9"
+              alt="Container"
+              src="https://c.animaapp.com/NlUgQSjM/img/container-1.svg"
+            />
+
+            <div className="ml-9 w-[306.66px] h-[28.59px] mt-6 flex">
+              <div className="mt-[1.5px] w-[202px] h-[29px] [font-family:'Space_Grotesk',Helvetica] font-bold text-[#2b2622] text-[22px] tracking-[-0.22px] leading-[28.6px] whitespace-nowrap">
+                5-min Daily Quizzes
+              </div>
+            </div>
+
+            <div className="ml-9 w-[306.66px] h-[51.19px] flex">
+              <p className="-mt-px w-[267px] h-[52px] [font-family:'Inter',Helvetica] font-normal text-[#6b6661] text-base tracking-[0] leading-[25.6px]">
+                Build a consistent practice habit with quick, bite-sized daily
+                tests.
+              </p>
+            </div>
+          </div>
+
+          <div className="w-[378.66px] h-[255.78px] flex flex-col bg-white rounded-3xl border-4 border-solid border-[#2b2622] shadow-[5px_5px_0px_#2b2622d9]">
+            <img
+              className="ml-9 w-20 h-20 relative mt-9"
+              alt="Container"
+              src="https://c.animaapp.com/NlUgQSjM/img/container-2.svg"
+            />
+
+            <div className="ml-9 w-[306.66px] h-[28.59px] mt-6 flex">
+              <div className="mt-[1.5px] w-[179px] h-[29px] [font-family:'Space_Grotesk',Helvetica] font-bold text-[#2b2622] text-[22px] tracking-[-0.22px] leading-[28.6px] whitespace-nowrap">
+                AI-Powered Hints
+              </div>
+            </div>
+
+            <div className="ml-9 w-[306.66px] h-[51.19px] flex">
+              <p className="-mt-px w-[289px] h-[52px] [font-family:'Inter',Helvetica] font-normal text-[#6b6661] text-base tracking-[0] leading-[25.6px]">
+                Get intelligent support and explanations whenever you&#39;re
+                stuck.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <p className="absolute top-[262px] left-[calc(50.00%_-_383px)] w-[767px] [font-family:'Space_Grotesk',Helvetica] font-bold text-black text-4xl text-center tracking-[-1.60px] leading-[80px] whitespace-nowrap">
+          Where DSA Learning becomes a habit
+        </p>
+      </div>
+    </div>
+  );
+};
